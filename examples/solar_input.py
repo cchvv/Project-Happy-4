a86ec2c3eae2e107db7647292224b7e2622466b8
Zihao Tao <taozihao@uw.edu>
2018-03-08 23:26:59 -0800

    file for example

---
+++ examples/solar_input.py
@@ -0,0 +1,102 @@
+import sys, os
+import sscapi
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+import math
+import requests
+
+def solar(lat, lon, y):
+    api_key = 'Mow538glfN0fjR9Sk9RFqnnhuUDm5lxZBcTTqeKK'
+    # Set the attributes to extract (e.g., dhi, ghi, etc.), separated by commas.
+    attributes = 'ghi,dhi,dni,wind_speed_10m_nwp,surface_air_temperature_nwp,solar_zenith_angle'
+    # Choose year of data
+    year = str(y)
+    # Set leap year to true or false. True will return leap day data if present, false will not.
+    leap_year = 'false'
+    # Set time interval in minutes, i.e., '30' is half hour intervals. Valid intervals are 30 & 60.
+    interval = '30'
+    # Specify Coordinated Universal Time (UTC), 'true' will use UTC, 'false' will use the local time zone of the data.
+    # NOTE: In order to use the NSRDB data in SAM, you must specify UTC as 'false'. SAM requires the data to be in the
+    # local time zone.
+    utc = 'false'
+    your_name = 'Zihao+Tao'
+    reason_for_use = 'beta+testing'
+    your_affiliation = 'university+of+washington'
+    your_email = 'taozihao@uw.edu'
+    mailing_list = 'true'
+    # Declare url string
+    url = 'http://developer.nrel.gov/api/solar/nsrdb_0512_download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=api_key, attr=attributes)
+    
+    req = requests.get(url)
+    assert req.status_code == 200, "The download failed, can not find this place's database."# if the download failed, this line will generate an error
+    # Return just the first 2 lines to get metadata:
+    info = pd.read_csv(url, nrows=1)
+    # See metadata for specified properties, e.g., timezone and elevation
+    timezone, elevation = info['Local Time Zone'], info['Elevation']
+    # Return all but first 2 lines of csv to get data:
+    df = pd.read_csv('http://developer.nrel.gov/api/solar/nsrdb_0512_download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=api_key, attr=attributes), skiprows=2)
+    # Set the time index in the pandas dataframe:
+    df = df.set_index(pd.date_range('1/1/{yr}'.format(yr=year), freq=interval+'Min', periods=525600/int(interval)))
+    ssc = sscapi.PySSC()
+    
+    # Resource inputs for SAM model:
+    wfd = ssc.data_create()
+    ssc.data_set_number(wfd, b'lat', lat)
+    ssc.data_set_number(wfd, b'lon', lon)
+    ssc.data_set_number(wfd, b'tz', timezone)
+    ssc.data_set_number(wfd, b'elev', elevation)
+    ssc.data_set_array(wfd, b'year', df.index.year)
+    ssc.data_set_array(wfd, b'month', df.index.month)
+    ssc.data_set_array(wfd, b'day', df.index.day)
+    ssc.data_set_array(wfd, b'hour', df.index.hour)
+    ssc.data_set_array(wfd, b'minute', df.index.minute)
+    ssc.data_set_array(wfd, b'dn', df['DNI'])
+    ssc.data_set_array(wfd, b'df', df['DHI'])
+    ssc.data_set_array(wfd, b'wspd', df['Wind Speed'])
+    ssc.data_set_array(wfd, b'tdry', df['Temperature'])
+    
+    # Create SAM compliant object 
+    dat = ssc.data_create()
+    ssc.data_set_table(dat, b'solar_resource_data', wfd)
+    ssc.data_free(wfd)
+    
+    # Specify the system Configuration
+    # Set system capacity in MW
+    system_capacity = 4
+    ssc.data_set_number(dat, b'system_capacity', system_capacity)
+    # Set DC/AC ratio (or power ratio). See https://sam.nrel.gov/sites/default/files/content/virtual_conf_july_2013/07-sam-virtual-conference-2013-woodcock.pdf
+    ssc.data_set_number(dat, b'dc_ac_ratio', 1.1)
+    # Set tilt of system in degrees
+    ssc.data_set_number(dat, b'tilt', 25)
+    # Set azimuth angle (in degrees) from north (0 degrees)
+    ssc.data_set_number(dat, b'azimuth', 180)
+    # Set the inverter efficency
+    ssc.data_set_number(dat, b'inv_eff', 96)
+    # Set the system losses, in percent
+    ssc.data_set_number(dat, b'losses', 14.0757)
+    # Specify fixed tilt system (0=Fixed, 1=Fixed Roof, 2=1 Axis Tracker, 3=Backtracted, 4=2 Axis Tracker)
+    ssc.data_set_number(dat, b'array_type', 0)
+    # Set ground coverage ratio
+    ssc.data_set_number(dat, b'gcr', 0.4)
+    # Set constant loss adjustment
+    ssc.data_set_number(dat, b'adjust:constant', 0)
+    
+    # execute and put generation results back into dataframe
+    mod = ssc.module_create(b'pvwattsv5')
+    ssc.module_exec(mod, dat)
+    df['generation'] = np.array(ssc.data_get_array(dat, b'gen'))
+    Daily = []
+    for i in range(int(len(df)/48)):
+        a = 0
+        for j in range(48):
+            a = a + df.iloc[int(i*48+j)]['generation']/system_capacity
+        Daily.append(a)
+    Daily = pd.DataFrame(Daily, columns=['generation'])
+    Daily_solar = Daily.set_index(pd.date_range('1/1/{yr}'.format(yr=year), freq='1'+'D', periods=len(df)/48))
+    # free the memory
+    ssc.data_free(dat)
+    ssc.module_free(mod)
+    generation_solar = df['generation'].sum()/system_capacity
+    
+    return Daily_solar,generation_solar
---
+++ examples/wind_input.py
@@ -0,0 +1,93 @@
+import sys, os
+import sscapi
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+import math
+import requests
+
+def wind(lat, lon, yr):
+    year = str(yr)
+    lat = str(lat)
+    lon = str(lon)
+    key = 'CNN87trTlIjPrPURSZM9VEUDnWzKulgUFtUVjUmr'
+    # Declare url string
+    url = 'http://developer.nrel.gov/api/wind/srw_aggregate_extract?api_key={APIKEY}&year={YEAR}&lat={LAT}&lon={LON}'.format(LAT=lat, LON=lon, YEAR=year, APIKEY=key);
+    
+    req = requests.get(url)
+    assert req.status_code == 200, "The download failed, can not find this place's database."# if the download failed, this line will generate an error
+    
+    with open("test.csv","wb") as f:
+        f.write(req.content)
+        
+    interval = '30'
+    df =pd.read_csv(url)
+    df = df[:][4:]
+    # Set the time index in the pandas dataframe:
+    df = df.set_index(pd.date_range('1/1/{yr}'.format(yr=year), freq=interval+'Min', periods=len(df)))
+    from PySSC import PySSC
+    system_capacity = 48000
+    ssc = PySSC()
+    
+    # Resource inputs for SAM model:
+    # Create SAM compliant object 
+    ssc.module_exec_set_print(0)
+    dat = ssc.data_create()
+    local = os.getcwd()+'/test.csv'
+    ssc.data_set_string( dat, b'wind_resource_filename', bytes(local, 'utf-8'));
+    ssc.data_set_number( dat, b'wind_resource_shear', 0.14000000059604645 )
+    ssc.data_set_number( dat, b'wind_resource_turbulence_coeff', 0.10000000149011612 )
+    # Specify the system Configuration
+    # Set system capacity in MW
+    ssc.data_set_number( dat, b'system_capacity', 48000 )
+    ssc.data_set_number( dat, b'wind_resource_model_choice', 0 )
+    ssc.data_set_number( dat, b'weibull_reference_height', 50 )
+    ssc.data_set_number( dat, b'weibull_k_factor', 2 )
+    ssc.data_set_number( dat, b'weibull_wind_speed', 7.25 )
+    ssc.data_set_number( dat, b'wind_turbine_rotor_diameter', 77 )
+    wind_turbine_powercurve_windspeeds =[ 0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 5, 5.25, 5.5, 5.75, 6, 6.25, 6.5, 6.75, 7, 7.25, 7.5, 7.75, 8, 8.25, 8.5, 8.75, 9, 9.25, 9.5, 9.75, 10, 10.25, 10.5, 10.75, 11, 11.25, 11.5, 11.75, 12, 12.25, 12.5, 12.75, 13, 13.25, 13.5, 13.75, 14, 14.25, 14.5, 14.75, 15, 15.25, 15.5, 15.75, 16, 16.25, 16.5, 16.75, 17, 17.25, 17.5, 17.75, 18, 18.25, 18.5, 18.75, 19, 19.25, 19.5, 19.75, 20, 20.25, 20.5, 20.75, 21, 21.25, 21.5, 21.75, 22, 22.25, 22.5, 22.75, 23, 23.25, 23.5, 23.75, 24, 24.25, 24.5, 24.75, 25, 25.25, 25.5, 25.75, 26, 26.25, 26.5, 26.75, 27, 27.25, 27.5, 27.75, 28, 28.25, 28.5, 28.75, 29, 29.25, 29.5, 29.75, 30, 30.25, 30.5, 30.75, 31, 31.25, 31.5, 31.75, 32, 32.25, 32.5, 32.75, 33, 33.25, 33.5, 33.75, 34, 34.25, 34.5, 34.75, 35, 35.25, 35.5, 35.75, 36, 36.25, 36.5, 36.75, 37, 37.25, 37.5, 37.75, 38, 38.25, 38.5, 38.75, 39, 39.25, 39.5, 39.75, 40 ];
+    ssc.data_set_array( dat, b'wind_turbine_powercurve_windspeeds',  wind_turbine_powercurve_windspeeds);
+    wind_turbine_powercurve_powerout =[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21.319999694824219, 33.509998321533203, 45.689998626708984, 65.209999084472656, 79.830001831054688, 104.25, 128.66000366210938, 157.97000122070312, 187.27000427246094, 216.58000183105469, 250.77999877929688, 292.32000732421875, 333.85000610351562, 375.39999389648438, 426.72000122070312, 475.60000610351562, 534.27001953125, 597.80999755859375, 656.489990234375, 724.94000244140625, 798.28997802734375, 871.6300048828125, 940.08001708984375, 1010, 1060, 1130, 1190, 1240, 1290, 1330, 1370, 1390, 1410, 1430, 1440, 1460, 1470, 1475, 1480, 1485, 1490, 1495, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+    ssc.data_set_array( dat, b'wind_turbine_powercurve_powerout',  wind_turbine_powercurve_powerout);
+    ssc.data_set_number( dat, b'wind_turbine_hub_ht', 80 )
+    ssc.data_set_number( dat, b'wind_turbine_max_cp', 0.44999998807907104 )
+    wind_farm_xCoordinates =[ 0, 616, 1232, 1848, 2464, 3080, 3696, 4312, 308, 924, 1540, 2156, 2772, 3388, 4004, 4620, 0, 616, 1232, 1848, 2464, 3080, 3696, 4312, 308, 924, 1540, 2156, 2772, 3388, 4004, 4620 ];
+    ssc.data_set_array( dat, b'wind_farm_xCoordinates',  wind_farm_xCoordinates);
+    wind_farm_yCoordinates =[ 0, 0, 0, 0, 0, 0, 0, 0, 616, 616, 616, 616, 616, 616, 616, 616, 1232, 1232, 1232, 1232, 1232, 1232, 1232, 1232, 1848, 1848, 1848, 1848, 1848, 1848, 1848, 1848 ];
+    ssc.data_set_array( dat, b'wind_farm_yCoordinates',  wind_farm_yCoordinates);
+    ssc.data_set_number( dat, b'wind_farm_losses_percent', 0 )
+    ssc.data_set_number( dat, b'wind_farm_wake_model', 0 )
+    ssc.data_set_number( dat, b'adjust:constant', 0 )
+    
+    # execute and put generation results back into dataframe
+    mod = ssc.module_create(b'windpower')	
+ 
+    ssc.module_exec_set_print( 0 );     
+    if ssc.module_exec(mod, dat) == 0:
+        print ('windpower simulation error')
+        idx = 1
+        msg = ssc.module_log(mod, 0)
+        while (msg != None):
+            print ('	: ' + msg.decode("utf - 8"))
+            msg = ssc.module_log(mod, idx)
+            idx = idx + 1
+        SystemExit( "Simulation Error" );    
+    
+    df = np.array(ssc.data_get_array(dat, b'gen')); 
+    df = pd.DataFrame(df,columns=['generation'])
+    df = df.set_index(pd.date_range('1/1/{yr}'.format(yr=year), freq=interval+'Min', periods=525600/60))
+    Daily=[]
+    for i in range(int(len(df)/24)):
+        a = 0
+        for j in range(24):
+            a = a + df.iloc[int(i*24+j)].values[0]/system_capacity
+        Daily.append(a)
+    Daily = pd.DataFrame(Daily, columns=['generation'])
+    Daily_wind = Daily.set_index(pd.date_range('1/1/{yr}'.format(yr=year), freq='1'+'D', periods=len(Daily)))
+    generation_wind = Daily_wind['generation'].sum()
+    # free the memory
+    ssc.data_free(dat)
+    ssc.module_free(mod)
+    os.remove('test.csv')
+       
+    return Daily_wind, generation_wind
